c813366c296369a46ee6f1ad38633e83b0ae073b
winetree94 <winetree94@gmail.com>
2023-04-10 12:54:24 +0900

    tab mapping

--- mappings.lua
+++ mappings.lua
@@ -6,23 +6,33 @@
 return {
   -- first key is the mode
   -- n = {
-    -- second key is the lefthand side of the map
-    -- mappings seen under group name "Buffer"
-    -- ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
-    -- ["<leader>bD"] = {
-    --   function()
-    --     require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
-    --   end,
-    --   desc = "Pick to close",
-    -- },
-    -- tables with the `name` key will be registered with which-key if it's installed
-    -- this is useful for naming menus
-    -- ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+  -- second key is the lefthand side of the map
+  -- mappings seen under group name "Buffer"
+  -- ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+  -- ["<leader>bD"] = {
+  --   function()
+  --     require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+  --   end,
+  --   desc = "Pick to close",
   -- },
+  -- tables with the `name` key will be registered with which-key if it's installed
+  -- this is useful for naming menus
+  -- ["<leader>b"] = { name = "Buffers" },
+  -- quick save
+  -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+  -- },
+  n = {
+    ["<S-Tab>"] = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    },
+    ["<Tab>"] = {
+      function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end,
+      desc = "Next buffer",
+    },
+  },
   -- t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
+  -- setting a mapping to false will disable it
+  -- ["<esc>"] = false,
   -- },
 }
