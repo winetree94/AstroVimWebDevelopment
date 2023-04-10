07a0c6c8c27dba9e9ce7f28f4369435fcb496119
winetree94 <winetree94@gmail.com>
2023-04-10 12:11:22 +0900

    chores: update preinstall

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,16 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = {
+        "html",
+        "cssls",
+        "cssmodules_ls",
+        "tailwindcss",
+        "tsserver",
+        "tailwindcss",
+        "angularls",
+        "eslint",
+      },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -13,14 +22,33 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      -- ensure_installed = { "prettier", "stylua" },
+      ensure_installed = {
+        "emmit-ls",
+        "prettier",
+        "prettierd",
+        "eslint_d",
+        "eslint-lsp",
+        "html-lsp",
+        "typescript-language-server",
+        "angular-language-server",
+        "css-lsp",
+        "cssmodules-language-server",
+        "tailwindcss-language-server",
+        "stylua",
+        "lua-language-server",
+        "lua-check",
+        "json-lsp",
+        "jsonlint",
+      },
     },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
-      -- ensure_installed = { "python" },
+      ensure_installed = {
+        "chrome",
+      },
     },
   },
 }
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = {
+      "lua",
+    },
   },
 }
