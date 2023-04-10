c660fed9e5ed5c49aaf8ea85fada8c3e95c6024f
winetree94 <winetree94@gmail.com>
2023-04-10 11:35:22 +0900

    chores: merge base config

--- init.lua
+++ init.lua
@@ -1,8 +1,23 @@
 return {
-  colorscheme = "catppuccin-mocha",
+    colorscheme = "catppuccin-mocha",
+
   lsp = {
     formatting = {
+      -- control auto formatting on save
+      format_on_save = {
+        enabled = true, -- enable or disable format on save globally
+        allow_filetypes = { -- enable format on save for specified filetypes only
+          -- "go",
+        },
+        ignore_filetypes = { -- disable format on save for specified filetypes
+          -- "python",
+        },
+      },
+      timeout_ms = 1000, -- default format timeout
       disabled = {
+        -- disable formatting capabilities for the listed language servers
+        -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
+        -- "lua_ls",
         "tsserver",
         "prettier",
         "html",
@@ -78,5 +93,34 @@ return {
         end,
       }
     end,
+    },
+
+  -- Configure require("lazy").setup() options
+  lazy = {
+    defaults = { lazy = true },
+    performance = {
+      rtp = {
+        -- customize default disabled vim plugins
+        disabled_plugins = { "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin", "tarPlugin" },
+      },
+    },
   },
+
+  -- This function is run last and is a good place to configuring
+  -- augroups/autocommands and custom filetypes also this just pure lua so
+  -- anything that doesn't fit in the normal config locations above can go here
+  polish = function()
+    -- Set up custom filetypes
+    -- vim.filetype.add {
+    --   extension = {
+    --     foo = "fooscript",
+    --   },
+    --   filename = {
+    --     ["Foofile"] = "fooscript",
+    --   },
+    --   pattern = {
+    --     ["~/%.config/foo/.*"] = "fooscript",
+    --   },
+    -- }
+  end,
 }
